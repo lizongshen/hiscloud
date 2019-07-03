1633f67df9dbf10b3aec1eb581b6c4d464ebd428
lizongshen <515672875@qq.com>
2019-07-03 23:48:59 +0800

    auto

--- menzhen/cmd/main.go
+++ menzhen/cmd/main.go
@@ -4,9 +4,9 @@ import "github.com/gin-gonic/gin"
 
 func main() {
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
+	r.GET("/jiuzhen", func(c *gin.Context) {
 		c.JSON(200, gin.H{
-			"message": "pong",
+			"message": "就诊成功",
 		})
 	})
 	r.Run() // listen and serve on 0.0.0.0:8080
